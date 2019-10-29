5c0adc51eea26f6f658bf738ad08da8f444394e9
ice_cookie <maximbu@mail.ru>
2019-10-29 17:54:36 +0300

    + class floor как, все под него заточил

---
+++ test1/Здание/Floor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    public class Floor
+    {
+        public int Number;
+
+        
+
+        public List<VertexWall> walls = new List<VertexWall>();
+        public List<VertexRoom> rooms = new List<VertexRoom>();
+        public List<VertexChain> chains = new List<VertexChain>();
+
+
+
+    }
+}
