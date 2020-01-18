68989fa14311f9f520b63955481d88032ae858c2
Pavel <paselyanin@yandex.ru>
2020-01-18 13:23:20 +0300

    Добавлены кнопки просмотра записей

--- test1/Redactor.Designer.cs
+++ test1/Redactor.Designer.cs
@@ -42,6 +42,9 @@
 			this.label5 = new System.Windows.Forms.Label();
 			this.label6 = new System.Windows.Forms.Label();
 			this.label7 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox8 = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
 			// textBox1
@@ -156,11 +159,42 @@
 			this.label7.TabIndex = 8;
 			this.label7.Text = "Адрес почты:";
 			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(4, 321);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Назад";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Visible = false;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(121, 321);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 10;
+			this.button2.Text = "Вперед";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Visible = false;
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(86, 323);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(29, 20);
+			this.textBox8.TabIndex = 11;
+			this.textBox8.Visible = false;
+			// 
 			// Redactor
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(210, 327);
+			this.ClientSize = new System.Drawing.Size(210, 346);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.label7);
 			this.Controls.Add(this.label6);
 			this.Controls.Add(this.label5);
@@ -199,5 +233,8 @@
 		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.Label label6;
 		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox8;
 	}
 }
